166d2120b5e68b493ec1258ecac905dbb87d56c8
Ruslana478 <ruslanapopovych2004@gmail.com>
2026-05-23 19:38:50 +0300

    Added protection from sending same data in case of not receiving it during for 5 minutes

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
 last_data = {}
 
 class SensorData(BaseModel):
-    temperature: float
-    humidity: float
-
-@app.get("/")
-def root():
-    return {"message": "Server works"}
+    temperature: float = Field(..., ge=-40, le=80)
+    humidity: float = Field(..., ge=0, le=100)
 
 @app.post("/sensor-data")
 def receive_data(data: SensorData):
+
     last_data["latest"] = {
         "temperature": data.temperature,
-        "humidity": data.humidity
+        "humidity": data.humidity,
+        "timestamp": datetime.now()
     }
 
     print(f"Received data: {last_data['latest']}", flush=True)
 
     return {
-        "status": "ok",
-        **last_data["latest"]
+        "status": "ok"
     }
 
 @app.get("/latest")
 def get_latest():
-    return last_data
+
+    if "latest" not in last_data:
+        return {
+            "temperature": 0,
+            "humidity": 0,
+            "status": "no data"
+        }
+
+    now = datetime.now()
+
+    last_timestamp = last_data["latest"]["timestamp"]
+
+    if now - last_timestamp > timedelta(minutes=5):
+        return {
+            "temperature": 0,
+            "humidity": 0,
+            "status": "device offline"
+        }
+
+    return {
+        "temperature": last_data["latest"]["temperature"],
+        "humidity": last_data["latest"]["humidity"],
+        "status": "online"
+    }
